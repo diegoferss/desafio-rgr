da90f6a65ee5e38c7f474c2e2b65399e73158ef2
Diego <diegosilva@jera.com.br>
2025-07-03 22:33:56 -0300

    [NEW] Add firebase core and initialize firebase on project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+import 'l10n/app_localizations.dart';
 import 'l10n/localize.dart';
 import 'support/services/injector/injector.dart';
-import 'l10n/app_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   injector.registerModules();
   runApp(const MyApp());
 }
@@ -20,4 +25,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
